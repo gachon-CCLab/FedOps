2ad0c71fcd74272ffda5d34468dd25edeb8baffa
Farkhod Yusubov <61786968+farhodyusupov@users.noreply.github.com>
2023-07-05 19:22:50 +0900

    fedOpsFlutter

---
+++ real_device/cross_device/fed_ops_flutter/android/src/main/java/com/example/fed_ops_flutter/TensorFlowTrain.java
@@ -0,0 +1,5 @@
+package com.example.fed_ops_flutter;
+
+public class TensorFlowTrain {
+
+}
---
+++ real_device/cross_device/fed_ops_flutter/example/android/app/src/main/java/com/example/fed_ops_flutter_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fed_ops_flutter_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
